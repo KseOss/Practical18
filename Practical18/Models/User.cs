1242f885831e0d5633752938310633ec325fe95c
KseOss <104015053+KseOss@users.noreply.github.com>
2025-04-18 10:44:27 +0300

    User.cs

--- Practical18/Models/User.cs
+++ Practical18/Models/User.cs
@@ -1,1 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace Practical18.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string UserSurname { get; set; } = null!;
+
+    public string UserName { get; set; } = null!;
+
+    public string? UserPatronymic { get; set; }
+
+    public string UserLogin { get; set; } = null!;
+
+    public string UserPassword { get; set; } = null!;
+
+    public int? UserRole { get; set; }
+
+    public virtual Role? UserRoleNavigation { get; set; }
+}
 
